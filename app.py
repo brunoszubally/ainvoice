4a3054fcf1d2797a2e0224a5994cd60578c64013
Szubally Bruno <szubally.bruno@epistogram.com>
2024-09-14 21:16:26 +0200

    reps

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai  # type: ignore
 import openai  # Itt az OpenAI modul helyes használata
